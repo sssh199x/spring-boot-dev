65d1ccf67e7bd39b8196125896deef89c588e1aa
sssh199x <sandeshhamal5890@gmail.com>
2025-12-27 19:06:46 +0545

    add: 01-spring-boot-demo,spring-boot-starters,spring-boot-parents for starters.

---
+++ 01-spring-boot-overview/01-spring-boot-demo/mycoolapp/src/main/java/com/sandesh/springboot/demo/mycoolapp/rest/RestFunController.java
@@ -0,0 +1,16 @@
+package com.sandesh.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestFunController {
+
+    @GetMapping("/" )
+        public static String sayHello(){
+            return "Hello World!";
+        }
+
+
+
+}
